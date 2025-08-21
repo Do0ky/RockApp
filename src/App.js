a64eeb505a363e5d1390619a6bfcbdcdd9c00216
Dooky <chamallow14@hotmail.fr>
2025-08-21 00:39:00 -0300

    restructured Homepage and removed mapping that is now in RockGallery

--- src/App.js
+++ src/App.js
@@ -1,37 +1,26 @@
-import RockCard from "./components/RockCard";
-import rocks from "./data/rocks.json"; // Importing the JSON data file
-
-//FAKE DATA TEST testing creating an array
-const rocksList = [
-  {
-    id: 0,
-    name: "Limestone",
-    category: "Sedimentary",
-    type: "Chemical"
-  },
-  {
-    id: 1,
-    name: "Sandstone",
-    category: "Sedimentary",
-    type: "Clastic"
-  },
-    {
-    id: 2,
-    name: "Chalk",
-    category: "Sedimentary",
-    type: "Organic"
-  }
-]
+import { useNavigate } from "react-router-dom";
+import './App.css'; // Importing the CSS file for styling
 
 function App() {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <h1 class="text-center">Rock App</h1>
-      {rocks.map( r => <RockCard key={r.id} rock={r} /> ) /* Mapping through the rocks array and creating a RockCard for each rock object */ }
+    <div className="homepage text-center">
+      <h1 className="text-center">Rock App</h1>
+
+        <div className="entry-options">
+
+          <div className='entry-card' onClick={() => navigate('/gallery')}>
+            <h2>⛰️ Enter Rock Gallery</h2>
+          </div>
+
+          <div className='entry-card' onClick={() => navigate('/quiz')}>
+            <h2>❓ Take the Rock Quiz</h2>
+          </div>
+
+        </div>
     </div>
   );
 }
-// rock is an attribute of RockCard => the parameter r is getting put into the prop called rock on the RockCard component
-// This r parameter will be given to each RockCard it creates by mapping, in a rock prop. And then it can be accessed in the RockCard component
 
 export default App;
