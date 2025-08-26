ff930d0300049e529b4eb9b9f2965b5f5116ddb5
SubparEscobar <aaron.escobar1997@gmail.com>
2025-08-26 00:09:00 -0500

    Added Footer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import { useNavigate } from "react-router-dom";
 import './App.css'; // Importing the CSS file for styling
+import Footer from "./components/Footer";
 
 function App() {
   const navigate = useNavigate();
 
   return (
-    <div className="homepage text-center">
-      <h1 className="text-center">Rock App</h1>
+    <div className="App">
+      <div className="homepage text-center">
+        <h1 className="text-center">Rock App</h1>
 
-        <div className="entry-options">
+          <div className="entry-options">
 
-          <div className='entry-card' onClick={() => navigate('/gallery')}>
-            <h2>⛰️ Enter Rock Gallery</h2>
+            <div className='entry-card' onClick={() => navigate('/gallery')}>
+              <h2>⛰️ Enter Rock Gallery</h2>
+            </div>
+
+            <div className='entry-card' onClick={() => navigate('/quiz')}>
+              <h2>❓ Take the Rock Quiz</h2>
+            </div>
+            
           </div>
-
-          <div className='entry-card' onClick={() => navigate('/quiz')}>
-            <h2>❓ Take the Rock Quiz</h2>
-          </div>
-
-        </div>
+      </div>
+      <Footer />
     </div>
   );
 }
