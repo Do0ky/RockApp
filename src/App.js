dca49a9d2785c90a2729b08af9dc4c08965857cf
SubparEscobar <aaron.escobar1997@gmail.com>
2025-08-30 00:57:05 -0500

    Added Header and CSS

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { useNavigate } from "react-router-dom";
 import './App.css'; // Importing the CSS file for styling
 import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 function App() {
   const navigate = useNavigate();
 
   return (
-    <div className="App">
+    <div className="app-container">
+      <Header />
       <div className="homepage text-center">
         <h1 className="text-center">Rock App</h1>
 
