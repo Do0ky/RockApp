97c8bb3f8e2c3fd211270e87e72904f65d20afc7
Dooky <chamallow14@hotmail.fr>
2025-09-06 16:31:04 -0300

    moved comments

--- src/pages/RockGallery.js
+++ src/pages/RockGallery.js
@@ -15,7 +15,6 @@ function RockGallery() {
     // Search state and fields
     const [query, setQuery] = useState("");
     
-
     // text search layered on the category-filtered list
     const  visibleRocks = useMemo(() => {
         const searchFields = ["name", "type", "category", "texture", ]; // from JSON
@@ -31,22 +30,25 @@ function RockGallery() {
     return (
         <div className="rock-gallery">
             <h2 className="text-center">Rock Gallery</h2>
+            
             <div className="d-flex justify-content-center my-3">
                 <RockSearch query={query} setQuery={setQuery} />
             </div>
 
             <div className="rock-gallery d-flex">
                 <FilterPanel onFilterChange={setActiveCategories} /> {/* Passing setActiveCategories function as a prop */}
+
                 <div className="d-flex flex-wrap justify-content-center flex-grow-1">
                     {/* Using filteredRocks to display only the rocks that match the active filters */}
                     {visibleRocks.map( r => <RockCard key={r.id} rock={r} /> )}
+                    {/* rock is an attribute of RockCard. The parameter r is getting put into the prop called rock on the RockCard component */}
+                    {/* This r parameter will be given to each RockCard it creates by mapping, in a rock prop. And then it can be accessed in the RockCard component */}
                 </div>
+
             </div>
 
         </div>
-    );
+    )
 };
-// rock is an attribute of RockCard => the parameter r is getting put into the prop called rock on the RockCard component
-// This r parameter will be given to each RockCard it creates by mapping, in a rock prop. And then it can be accessed in the RockCard component
 
 export default RockGallery;
