f9154c92172a2ee800dad2c047e78728b47b2f59
Dooky <chamallow14@hotmail.fr>
2025-09-07 21:49:22 -0300

    Reorganized divs to make a purely visual layer

--- src/App.js
+++ src/App.js
@@ -1,29 +1,38 @@
-import { useNavigate } from "react-router-dom";
-import './App.css'; // Importing the CSS file for styling
-import Footer from "./components/Footer";
+import './App.css';
 import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { useNavigate } from "react-router-dom";
 
 function App() {
   const navigate = useNavigate();
 
   return (
     <div className="app-container">
-      <Header />
-        <div className="home-background homepage text-center">
 
-            <div className="entry-options">
+      <div className="home-background"> {/* purely visual layer */}
 
-              <div className='entry-card' onClick={() => navigate('/gallery')}>
-                <h2>Enter Rock Gallery</h2>
+        <Header />
+
+          <div className="homepage text-center"> {/* main content layer */}
+
+              <div className="entry-options"> {/* flex container entry cards */}
+
+                <div className='entry-card' onClick={() => navigate('/gallery')}>
+                  <h2>Enter Rock Gallery</h2>
+                </div>
+
+                <div className='entry-card' onClick={() => navigate('/quiz')}>
+                  <h2>Take the Rock Quiz</h2>
+                </div>
+                
               </div>
 
-              <div className='entry-card' onClick={() => navigate('/quiz')}>
-                <h2>Take the Rock Quiz</h2>
-              </div>
-              
-            </div>
-        </div>
+          </div>
+          
+      </div>
+
       <Footer />
+
     </div>
   );
 }
